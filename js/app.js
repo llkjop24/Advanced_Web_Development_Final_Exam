2ee9db19662a94103372041498bc222b8cac2cff
Jiwon <llkjop24@gmail.com>
2017-12-18 18:09:20 +0900

    새노트, 노트 저장 버튼 이벤트

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log('my-note app.js');
+
+window.onload = function (ev) {
+    text = document.getElementById('memo').value = localStorage.getItem('text');
+}
+
+newBtn = document.querySelector('.btn-newnote');
+
+console.log(newBtn);
+
+newBtn.addEventListener('click', function() {
+    document.getElementById('memo').value = '';
+})
+
+saveBtn = document.querySelector('.btn-savenote');
+
+saveBtn.addEventListener('click', function() {
+    localStorage.text = document.getElementById('memo').value;
+})
+
+
